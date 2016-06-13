3d86f2b4ce1d9eeb493067635493c80913154fb2
okmttdhr <10101114fin@gmail.com>
2016-06-13 10:33:42 +0900

    [add]delegateの処理をプラグイン側に持たせる

--- index.ios.ts
+++ index.ios.ts
@@ -1,9 +1,30 @@
 import applicationModule = require("application");
 import { IFacebookLoginHandler } from "./index.d";
 
+declare const FBSDKAppEvents: any;
+declare const FBSDKApplicationDelegate: any;
 declare const FBSDKLoginManager: any;
 declare type FBSDKLoginManagerLoginResult = any;
+
 declare type NSError = any;
+export declare class UIResponder { any; };
+export declare class UIApplicationDelegate { any; };
+export declare type UIApplication = any;
+export declare type NSDictionary = any;
+
+export class MyDelegate extends UIResponder implements UIApplicationDelegate {
+  public static ObjCProtocols = [UIApplicationDelegate];
+
+  applicationDidFinishLaunchingWithOptions(application: UIApplication, launchOptions: NSDictionary): boolean {
+    return FBSDKApplicationDelegate.sharedInstance().applicationDidFinishLaunchingWithOptions(application, launchOptions);
+  }
+  applicationOpenURLSourceApplicationAnnotation(application, url, sourceApplication, annotation) {
+    return FBSDKApplicationDelegate.sharedInstance().applicationOpenURLSourceApplicationAnnotation(application, url, sourceApplication, annotation);
+  }
+  applicationDidBecomeActive(application: UIApplication): void {
+    FBSDKAppEvents.activateApp();
+  }
+}
 
 export class FacebookLoginHandler implements IFacebookLoginHandler {
   private isInit: boolean = false;
--- sample/app/app.ts
+++ sample/app/app.ts
@@ -1,26 +1,7 @@
 ﻿import application = require("application");
-
-declare class UIResponder { any; };
-declare class UIApplicationDelegate { any; };
-declare type UIApplication = any;
-declare type NSDictionary = any;
-declare const FBSDKApplicationDelegate: any;
-declare const FBSDKAppEvents: any;
+const MyDelegate = require("nativescript-facebook-login2").MyDelegate;
 
 if (application.ios) {
-  class MyDelegate extends UIResponder implements UIApplicationDelegate {
-    public static ObjCProtocols = [UIApplicationDelegate];
-
-    applicationDidFinishLaunchingWithOptions(application: UIApplication, launchOptions: NSDictionary): boolean {
-      return FBSDKApplicationDelegate.sharedInstance().applicationDidFinishLaunchingWithOptions(application, launchOptions);
-    }
-    applicationOpenURLSourceApplicationAnnotation(application, url, sourceApplication, annotation) {
-      return FBSDKApplicationDelegate.sharedInstance().applicationOpenURLSourceApplicationAnnotation(application, url, sourceApplication, annotation);
-    }
-    applicationDidBecomeActive(application: UIApplication): void {
-      FBSDKAppEvents.activateApp();
-    }
-  }
   application.ios.delegate = MyDelegate;
 }
 
